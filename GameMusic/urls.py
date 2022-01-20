6d7a2b1c8ce6f62cd1cb37404c5d62dc5a617072
heckfyxe <heckfyxe@gmail.com>
2022-01-20 21:44:35 +0300

    Added authorization endpoints. Fixed static files

--- GameMusic/urls.py
+++ GameMusic/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from mysite.views import index
+from mysite.views import *
 
 urlpatterns = [
-    path('', index),
-]
+    path('', index, name='index'),
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
