eda09c5549bbb9c4220225bc9ebc3c71788cb924
heckfyxe <heckfyxe@gmail.com>
2022-01-20 23:11:25 +0300

    Fixed registration

--- mysite/views.py
+++ mysite/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from mysite.models import Profile
+
 
 def index(request):
     return render(request, "index.html")
@@ -10,13 +12,14 @@ def index(request):
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and form.data['role']:
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
+            Profile(user=user, role=form.data['role']).save()
             login(request, user)
-            return redirect('home')
+            return redirect('index')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
